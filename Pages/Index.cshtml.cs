300738e111a850e521596950620bee33c3b3f5b4
Alex <amatulionis@virtual-corp.com>
2019-03-30 12:52:28 -0700

    2/3 features complete. Removed bin & obj folders.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,40 +8,81 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+//
+// Summary:
+//     The main webapp namespace for binding operations.
 namespace activePdfApp.Pages
 {
+    //
+    // Summary:
+    //     The main webapp class model for binding operations.
     public class IndexModel : PageModel
     {
+        //
+        // Summary:
+        //     The image file to be uploaded by the user (one at a time).
         [BindProperty]
         public IFormFile Image { get; set; }
-
+        //
+        // Summary:
+        //     The relative path to the generated PDF of the image uploaded.
         public string PDF = "";
-
+        //
+        // Summary:
+        //     The main handler for on-upload actions of image files.
         public async Task<IActionResult> OnPostAsync()
         {
-            Console.WriteLine($"Uploaded Image: {Image.FileName}.");
+            Console.WriteLine($"Uploaded Image: {Image.Name()}");
 
             var TK = new APToolkitNET.Toolkit();
 
             var imageFile = await SaveFile(Image);
-            PDF = $"{Image.FileName}.pdf";
-
-            var res = TK.ImageToPDF(imageFile, PDF);
+            var pdf = $"{Image.Name()}.pdf";
+            var res = TK.ImageToPDF(imageFile, "wwwroot/" + pdf);
 
             Console.WriteLine($"TK.ImageToPDF = {res}");
 
+            PDF = pdf;
+
             return Page();
         }
-
+        //
+        // Summary:
+        //     Saves a file in memory to the webapp's root folder for frontend access.
         private static async Task<string> SaveFile(IFormFile file)
         {
             if (file.Length == 0) return "";
 
-            using (var fileStream = new FileStream(file.FileName, FileMode.Create))
+            using (var fileStream = new FileStream("wwwroot/" + file.Name(), FileMode.Create))
             {
                 await file.CopyToAsync(fileStream);
-                return file.FileName;
+                return "wwwroot/" + file.Name();
             }
         }
     }
+    //
+    // Summary:
+    //     Holds extension methods for convenience specific to the webapp.
+    public static class MyExtensions
+    {
+        // Summary:
+        //     Sanitizes a path in a IFormFile's FileName to be just the file name itself.
+        public static string Name(this IFormFile file)
+        {
+            var fileName = file.FileName;
+
+            var slash = fileName.LastIndexOf('\\');
+            if (slash == -1)
+            {
+                slash = fileName.LastIndexOf('/');
+            }
+
+            if (slash != -1)
+            {
+                fileName = fileName.Substring(slash + 1);
+            }
+
+            return fileName;
+        }
+    } 
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,30 +3,57 @@
 
 // Write your Javascript code.
 
-// Open selected PDF
-function initReaderPlus() {
-  let file = document.getElementById('file-upload').files[0];
+// Open selected PDF.
+function initReaderPlus(pdf) {
+  console.log('initReaderPlus('+pdf+')!');
 
-  if (file.name.indexOf("pdf") == -1) {
+  // let file = document.getElementById('file-upload').files[0];
+
+  if (pdf.indexOf("pdf") == -1) {
+    console.log('initReaderPlus: is not pdf');
     return;
   }
+
+  fetch(pdf).then(resp => {
+
+    console.log('initReaderPlus: fetched ' + pdf);
+    return resp.blob();
+
+  }).then(blob => {
+
+    console.log('initReaderPlus: blobified!');
+    blob.name = pdf;
+    readerPlus(blob);
+
+  }).catch(reas => {
+    console.error('initReaderPlus: ' + reas);
+  });
+}
+
+// Process a PDF blob.
+function readerPlus(file) {
+  console.log('readerPlus('+file.name+')!');
   
-  // Load document into memory
+  // Load document into memory.
   let reader = new FileReader();
 
   reader.readAsDataURL(file);
 
-  // Shared between onload and onloadend for the reader
+  // Shared between onload and onloadend for the reader.
   let data;
 
   reader.onload = function(e) {
+    console.log('readerPlus: onload');
+
     let filedata = e.target.result;
     data = filedata.substring(28);
   }
 
-  // Initialization settings
+  // Initialization settings.
   reader.onloadend = function(e) {
-    document.getElementById('file-upload').outerHTML = "";
+    console.log('readerPlus: onloadend');
+    
+    // document.getElementById('file-upload').outerHTML = "";
 
     readerplus.initializeSettings({
       protocol: "http",
@@ -38,42 +65,44 @@ function initReaderPlus() {
     readerplus.Document.addEventListener("load", function () {
       // On document load you can add addtional options such as 
       // adding annotations, populating form fields and controlling 
-      // elements of the user interface
+      // elements of the user interface.
     });
 
-    // Document settings
+    // Document settings.
     let isMasterDocument = 1;
     let editMode = 1;
 
-    // Upload document from memory into viewer
-    let result = readerplus.Document.upload(data, isMasterDocument, editMode, "", "DocumentName.pdf");
+    // Upload document from memory into viewer.
+    let result = readerplus.Document.upload(data, isMasterDocument, editMode, "", file.name);
 
     if (result.Status === 0) {
-      // Save document ID in order to reopen a document from the Reader Plus data store
+      // Save document ID in order to reopen a document from the Reader Plus data store.
       let docID = readerplus.Document.getDocumentID();
-      // Open document in edit mode
+      // Open document in edit mode.
       readerplus.Document.edit(docID);
     } else {
-      // Display alert on error
+      // Display alert on error.
       alert('Document failed to open!');
       console.error(result.Details);
     }
 
   };
 
-  // Document can be saved to any location when it is submitted by a user
+  // Document can be saved to any location when it is submitted by a user.
   readerplus.Document.addEventListener("submit", function(strResult) {
+    console.log('readerPlus: submit');
+    
     let result = JSON.parse(strResult);
 
     if (result.Status === 0) {
-      // Open the submitted document in another tab and redirect to thankyou.html
+      // Open the submitted document in another tab and redirect to thankyou.html.
       let submittedPDFData = result.Details;
       let pdfwindow = window.open("");
 
       pdfwindow.document.write("<iframe width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(submittedPDFData) + "'></iframe>");
-      window.location = "thankyou.html";
+      // window.location = "thankyou.html";
     } else {
-      // Display alert on error
+      // Display alert on error.
       alert('Document failed to submit!');
       console.error(result.Details);
     }
